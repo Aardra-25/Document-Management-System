88f78368b21b8f65230340b795a1dfb1ffdd2579
asherantonylouis <asherantony77@gmail.com>
2025-06-26 10:35:50 +0530

    initiate commit

---
+++ frontend-react-application/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>DOCUMENT MANAGEMENT SYSTEM</h1>
+    </div>
+  );
+}
+
+export default App;
