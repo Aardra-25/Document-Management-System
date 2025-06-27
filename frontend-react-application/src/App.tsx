fc1c3b5f23069467efff91740efbe138c269be52
Asher Antony Louis <145423330+asherantonylouis@users.noreply.github.com>
2025-06-27 16:50:36 +0530

    Update App.tsx

--- frontend-react-application/src/App.tsx
+++ frontend-react-application/src/App.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import "./App.css"; 
+import LandingPage from "./LandingPage";
+import EditDocumentPage from "./EditDocumentPage";
+import ViewDocumentPage from "./ViewDocumentpage";
+import DeletePage from "./DeletePage";
 
-import DeletePage from './DeletePage';
+import "./App.css";
+import AddDoc from "./pages/AddDoc";
+
 function App() {
-
-    </div>
-  );
-
   return (
-   
-
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/add" element={<AddDoc/>} />
+        <Route path="/edit" element={<EditDocumentPage />} />
+        <Route path="/view" element={<ViewDocumentPage />} />
+        <Route
+          path="/delete"
+          element={
+            <div style={{ justifyContent: "right", alignItems: "center", marginTop: "0%", marginRight: 40 }}>
+              <DeletePage />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
 export default App;
