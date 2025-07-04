1e6d866322530cc6f475d685bfc26cdaa43d9cc8
ananya477 <ananyajos747@gmail.com>
2025-07-04 12:39:17 +0530

    added link to data retrieval

--- ai-python-application/api/endpoints.py
+++ ai-python-application/api/endpoints.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter
 from schemas.schemas import InquiryRequest, LinkResponse
-from services.document_service.document_service_client import DocumentServiceClient 
+from services.document_service.document_service_client import DocumentServiceClient
 
 router = APIRouter()
 document_service_client = DocumentServiceClient()
+
 @router.post("/inquiry", response_model=LinkResponse)
 def inquiry(request: InquiryRequest):
     return {"link": "Chatbot will start soon"}
 
+@router.post("/link/{project_name}", response_model=LinkResponse)
+async def document1(request: InquiryRequest, project_name: str):
+    java_response, html_content = await document_service_client.fetch_document(project_name)
+    return {"link": html_content}
+
 @router.post("/document/{project_name}", response_model=LinkResponse)
 async def document(request: InquiryRequest, project_name: str):
-    response_data = await document_service_client.fetch_document(project_name)
-    print(response_data)
-    return {"link": f"{response_data}: It will be saved"}
+    java_response, html_content = await document_service_client.fetch_document(project_name)
+    return {"link": f"{java_response}: It will be saved"}
+
