9dc3a190f053b641fb36596111b0747fa7bf407e
Aardra-25 <aardrarb@gmail.com>
2025-07-08 16:35:57 +0530

    apiadded

--- Backend-Java-Application/src/main/java/com/litmus/Backend_Java_Application/model/Document.java
+++ Backend-Java-Application/src/main/java/com/litmus/Backend_Java_Application/model/Document.java
@@ -2,18 +2,47 @@ package com.litmus.Backend_Java_Application.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Entity
 public class Document {
     // Getters and setters
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column
+    private String documentName;
+    @Column
+    private String filePath;
 
-    public void setId(Long id) { this.id = id; }
 
-    public void setName(String name) { this.name = name; }
+    public void setName(String document_name) {
+        this.documentName = document_name;
+    }
+
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName;
+    }
+    public void setFile(String file_path){
+        this.filePath = file_path;
+    }
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+
 }
+
+
