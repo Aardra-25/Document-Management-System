8864df21222e8fa487d7e739f8af0173cce32205
Aardra-25 <aardrarb@gmail.com>
2025-07-10 12:13:00 +0530

    all API endpoints added

--- Backend-Java-Application/src/main/java/com/litmus/Backend_Java_Application/controller/DocumentController.java
+++ Backend-Java-Application/src/main/java/com/litmus/Backend_Java_Application/controller/DocumentController.java
@@ -3,9 +3,11 @@ package com.litmus.Backend_Java_Application.controller;
 import com.litmus.Backend_Java_Application.model.Document;
 import com.litmus.Backend_Java_Application.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/documents")
@@ -23,18 +25,33 @@ public class DocumentController {
     @GetMapping("/get-by-ids")
     public List<Document> findAllById(@RequestParam List<Long> ids) {
         return documentService.findAllById(ids);
-
     }
 
-   /* @PostMapping("/new")
+    @PostMapping("/new")
     public Document addDocument(@RequestBody Document document) {
         return documentService.saveDocument(document);
     }
-*/
+
     @DeleteMapping("/{id}")
     public String deleteDocument(@PathVariable Long id) {
         return documentService.deleteById(id);
     }
+
+    @PutMapping("/document/{id}")
+    public ResponseEntity<Document> updateDocument(@PathVariable Long id, @RequestBody Document updatedDocument) {
+        Optional<Document> existing = documentService.findById(id);
+        if (existing.isPresent()) {
+            Document document = existing.get();
+            document.setDocumentName(updatedDocument.getDocumentName());
+            document.setFilePath(updatedDocument.getFilePath());
+
+            Document saved = documentService.saveDocument(document);
+            return ResponseEntity.ok(saved);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 
 
     @GetMapping("/projectName/{documentName}")
